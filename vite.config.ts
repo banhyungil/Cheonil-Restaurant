357205c8a773dedb3f24f54013f25406e1659c31
banhyungil <byban4186@gmail.com>
2024-01-08 23:28:41 +0900

    feat: scss 개발환경 설정

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,24 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   server: {
     proxy: {
-        '/app': 'http://127.0.0.1:3000'
-    }
-  }
+      '/app': 'http://127.0.0.1:3000',
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+            @import "@/scss/main.scss";
+        `,
+      },
+    },
+  },
 })
