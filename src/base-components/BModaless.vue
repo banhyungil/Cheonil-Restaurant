e5249f0d957c1074603f1820db6e8ee57f2098d4
banhyungil <byban4186@gamil.com>
2025-06-28 11:46:02 +0900

    refactor: popup 기능 추가

--- src/base-components/BModaless.vue
+++ src/base-components/BModaless.vue
@@ -1,67 +1,97 @@
 <script setup lang="ts">
-import { useDraggable } from '@vueuse/core'
+import { useDraggable, useEventListener } from '@vueuse/core'
 import _ from 'lodash'
 
 defineOptions({
     inheritAttrs: false,
 })
 
-// Props
-interface Props {
+//ANCHOR - Props
+export interface BModalessProps {
     title?: string
     activeClose?: boolean
+    activeCloseClickOutside?: boolean
+    initPosition?: { x: number; y: number }
+    container?: HTMLElement | string
+    visible?: boolean
 }
-withDefaults(defineProps<Props>(), {
+const props = withDefaults(defineProps<BModalessProps>(), {
     title: '',
     activeClose: true,
+    activeCloseClickOutside: false,
+    container: 'body',
+    visible: true,
 })
 
+//ANCHOR - Models
+const isOpen = defineModel<boolean>({ required: true })
+
+//ANCHOR - Emits
 const emit = defineEmits<{
     close: []
 }>()
 
 //ANCHOR - Start
-onMounted(() => window.addEventListener('keydown', handleKeyDown))
-onUnmounted(() => window.removeEventListener('keydown', handleKeyDown))
+onUnmounted(() => cleanup())
 
 const bmodalessEl = ref<HTMLElement | null>(null)
+const cleanup = useEventListener(bmodalessEl, 'keydown', handleKeyDown)
 const titlebarEl = ref<HTMLElement | null>(null)
 const { x, y, style } = useDraggable(bmodalessEl, { handle: titlebarEl })
 
-// v-model
-const isOpen = defineModel<boolean>({ required: true })
+// Click Outside 닫기
+useClickOutside({
+    target: bmodalessEl,
+    callback: () => {
+        isOpen.value = false
+    },
+    disabled: computed(() => props.activeCloseClickOutside == false),
+})
 
 // ESC 키 닫기 (선택)
-const handleKeyDown = (e: KeyboardEvent) => {
+function handleKeyDown(e: KeyboardEvent) {
     if (e.key === 'Escape') isOpen.value = false
 }
 
-watch(isOpen, async () => {
-    if (isOpen.value) {
-        // element가 준비되기 전에 위치 조정이 일어날 수 있으므로, 다음 틱에서 실행
-        await nextTick()
+onMounted(() => {
+    watch(
+        isOpen,
+        async () => {
+            if (isOpen.value) {
+                // element가 준비되기 전에 위치 조정이 일어날 수 있으므로, 다음 틱에서 실행
+                await nextTick()
 
-        // 모달이 열릴 때 위치 조정
-        x.value = window.innerWidth / 2 - bmodalessEl.value!.offsetWidth / 2
-        y.value = window.innerHeight / 2 - bmodalessEl.value!.offsetHeight / 2
-    } else {
-        emit('close')
-    }
+                // 모달이 열릴 때 위치 조정
+                if (props?.initPosition) {
+                    x.value = props.initPosition.x
+                    y.value = props.initPosition.y
+                } else {
+                    x.value = window.innerWidth / 2 - bmodalessEl.value!.offsetWidth / 2
+                    y.value = window.innerHeight / 2 - bmodalessEl.value!.offsetHeight / 2
+                }
+            } else {
+                emit('close')
+            }
+        },
+        { immediate: true }
+    )
 })
 </script>
 
 <template>
-    <Teleport to="body">
+    <Teleport :to="container">
         <transition name="fade">
             <div
                 v-if="isOpen"
+                v-show="visible"
+                tabindex="0"
                 ref="bmodalessEl"
                 class="bmodaless fixed z-10 flex h-[30vh] w-[30vw] flex-col bg-dark p-2 text-white"
                 v-bind="_.pick($attrs, ['style', 'class'])"
                 :style="style"
             >
                 <!-- Title + 닫기 버튼 -->
-                <div ref="titlebarEl" class="titlebar mb-2 flex items-center justify-between border-b-2 border-lightgray pb-1">
+                <div ref="titlebarEl" class="titlebar mb-2 flex cursor-move items-center justify-between border-b-2 border-lightgray pb-1">
                     <slot name="title">
                         <h2 class="ml-2 text-lg font-bold">{{ title }}</h2>
                     </slot>
@@ -76,7 +106,7 @@ watch(isOpen, async () => {
                 </div>
 
                 <!-- 푸터 -->
-                <div class="mt-2 flex justify-end border-t-2 border-lightgray pt-1">
+                <div class="mt-2 flex justify-end border-t-2 border-lightgray pb-2 pt-4">
                     <slot name="footer" />
                 </div>
             </div>
@@ -89,6 +119,7 @@ watch(isOpen, async () => {
     .content {
         @apply overflow-y-auto;
     }
+
     // empty
 }
 </style>
